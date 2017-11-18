81d34739ad5a2d1ed6f6512bc2a86efcf8620a24
Sean Pollock <seanpollock3344@gmail.com>
2017-11-17 19:20:01 -0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={appStyle}>
+        <div style={introStyle}>
+          <h3>hi, my name is sean.</h3>
+          <h3>thanks for stoping by. here are some links to wander down.</h3>
+          <h3><a href='https://github.com/SeanAvery'>github</a></h3>
+          <h3><a href='https://twitter.com/breadofsean'>twitter</a></h3>
+          <h3><a href='https://www.instagram.com/seanpaul3344/'>instagram</a></h3>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+const appStyle = {
+  fontFamily: 'Roboto',
+  display: 'flex',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
+  color: '#383838'
+}
+
+const introStyle = {
+  padding: 30,
+  width: 800,
+  height: 500
+}
